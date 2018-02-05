0650cc677bdb6b47a0c66d0067d3e566ff4c7b5f
Travis Clarkson <tk1667@gmail.com>
2018-02-05 01:00:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  Object.assign({}, object, [key])
+  Object.assign(object, object, [key])
   return object
 }
